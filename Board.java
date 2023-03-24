53ee4b29fef9741c010430c1da78f9e1b00eb27d
Codevs-py <66708802+Codevs-py@users.noreply.github.com>
2023-03-25 01:46:07 +0530

    Made bg permanent

--- Board.java
+++ Board.java
@@ -2,11 +2,16 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Board extends JPanel {
-    Image image;
+    ImageIcon image;
     Caret player1, player2;
     Board(Caret player1, Caret player2){
-        image  = new ImageIcon("Images/Background.png").getImage();
+        image  = new ImageIcon("Images/Background.png");
         this.player1 = player1;
+
+        JLabel label = new JLabel("", image, JLabel.CENTER);
+        label.setBounds(0,0,480,830);
+        this.add(label);
+        label.setVisible(true);
 
         this.setPreferredSize(new Dimension(480,830));
         this.setOpaque(false);
@@ -16,13 +21,13 @@ public class Board extends JPanel {
         this.player1.setBounds(player1.x,player1.y, 150,150);
         this.add(player2);
         this.setVisible(true);
+
     }
     @Override
     public void paintComponent(Graphics g) {
         Graphics2D canvas = (Graphics2D)g;
         player1.setLocation(player1.x,player1.y);
 
-        canvas.drawImage(image,0,0,Const.WIDTH, Const.HEIGHT, null);
-//        canvas.drawImage(player1.image, player1.x, player1.y, 150, 150, null);
+//        canvas.drawImage(image,0,0,Const.WIDTH, Const.HEIGHT, null);
     }
 }
